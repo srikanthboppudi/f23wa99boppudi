5136c8d7359ffaa7140bb4305f5c96c9482861ed
srikanthboppudi <srikanthboppudi4@gmail.com>
2023-10-24 14:10:20 -0500

    c

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+var more=0;
+var sum=0;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more=more+1;
+  sum=sum+more;
+  res.send('Sum is: '+sum);
 });
 
 module.exports = router;
