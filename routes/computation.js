fd995af8d79e39bd7613b55f17d144a22619e64c
srikanthboppudi <srikanthboppudi4@gmail.com>
2023-10-24 19:34:04 -0500

    c

--- routes/computation.js
+++ routes/computation.js
@@ -1,11 +1,11 @@
 var express = require("express");
 var router = express.Router();
 router.get("/", function (req, res, next) {
- var randNumArr = Math.random() * 2;
- var rndNumArr = Math.round(randNumArr);
+ var randNumA = Math.random() * 2;
+ var rndNumA = Math.round(randNumA);
  if (req.query.x) {
   let x = req.query.x;
-  var arr = [Math.abs(x), Math.acos(x), Math.sin(x),Math.sinh(x)];
+  var arr = [Math.abs(x), Math.acos(x), Math.sin(x), Math.sinh(x)];
   var funObj = {
    0: "Math.abs()",
    1: "Math.acos()",
@@ -14,12 +14,12 @@ router.get("/", function (req, res, next) {
   };
   var arr = [Math.abs(x), Math.acos(x), Math.sin(x), Math.sinh(x)];
   let response1 = `
-  Function ${funObj[rndNumArr]} applied to x: ${x} the result is ${arr[rndNumArr].toFixed(4)}`;
+  Function ${funObj[rndNumA]} applied to x: ${x} the result is ${arr[rndNumA].toFixed(4)}`;
   res.send(response1);
  }
  else {
-  var randNumArr = Math.random() * 2;
-  var rndNumArr = Math.round(randNumArr);
+  var randNumA = Math.random() * 2;
+  var rndNumA = Math.round(randNumA);
   var randNum = Math.random() * 10;
   var x = Math.round(randNum);
   var funObj = {
@@ -28,9 +28,9 @@ router.get("/", function (req, res, next) {
    2: "Math.sin()",
    3: "Math.sinh()",
   };
-  var arr = [Math.abs(x), Math.acos(x), Math.sin(x), Math.sinh(x)];
+  var arr = [Math.abs(x), Math.acos(x), Math.sin(x), Math.sinh()];
   let response = `
-  Function ${funObj[rndNumArr]} applied to the random value for x from 1 to 10 is: ${x} and the result is ${arr[rndNumArr].toFixed(4)}`
+  Function ${funObj[rndNumA]} applied to the random value for x from 1 to 10 is: ${x} and the result is ${arr[rndNumA].toFixed(4)}`
   res.send(response);
  }
 });
